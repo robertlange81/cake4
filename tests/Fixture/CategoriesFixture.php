67ebfe7be8c062b25c9325898298cb91316fded5
Robert Lange <robert.lange@uni-leipzig.de>
2024-04-15 09:33:58 +0200

    init with bin/cake bake all categories

---
+++ tests/Fixture/CategoriesFixture.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * CategoriesFixture
+ */
+class CategoriesFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'name' => 'Lorem ipsum dolor sit amet',
+                'created' => '2024-04-15 07:32:45',
+                'modified' => '2024-04-15 07:32:45',
+            ],
+        ];
+        parent::init();
+    }
+}
