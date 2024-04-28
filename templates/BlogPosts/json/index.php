538566f583593a30a1851c9ab0a8f01dd7632744
Robert Lange <robert.lange.81@gmail.com>
2024-04-28 23:11:20 +0200

    hint for REST

--- templates/BlogPosts/json/index.php
+++ templates/BlogPosts/json/index.php
@@ -8,5 +8,6 @@
 foreach ($blogPosts as $blogPost) {
     unset($blogPost->modified);
     $blogPost->customNow = new DateTime('now', new DateTimeZone('Europe/Berlin'));
+    // before CakePHP 4.4 you need to use the request handler component
 }
 echo json_encode(compact('blogPosts'));
