124a891d544e98e670ea40b45ea35c4c96c944f9
Robert Lange <robert.lange.81@gmail.com>
2024-04-28 22:04:25 +0200

    add own form helper

--- src/View/AppView.php
+++ src/View/AppView.php
@@ -38,6 +38,7 @@ class AppView extends View
     public function initialize(): void
     {
         $this->loadHelper("Form", [
+            "className"=>'MyForm',
             "templates"=>'app_form'
         ]);
     }
