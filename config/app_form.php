c33e194208f88190dfe11ea44db60ea53657601e
Robert Lange <robert.lange.81@gmail.com>
2024-04-28 22:19:58 +0200

    add own form helper

---
+++ config/app_form.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'inputContainer' => '<div class="input {{type}}{{required}} {{customClass}} xxx">{{content}}</div>',
+    'input' => '<input type="{{type}}" name="{{name}}"{{attrs}}/>',
+    'requiredClass' => 'required'
+];
---
+++ src/View/Helper/MyFormHelper.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types= 1);
+
+namespace App\View\Helper;
+
+use Cake\View\Helper\FormHelper;
+
+class MyFormHelper extends FormHelper {
+    public function control(string $fieldName, array $options = []): string
+    {
+        return 'Test FormHelper' . parent::control($fieldName, $options);
+    }
+}
