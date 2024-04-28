bfbd085795050d044677b3bf79d96b9727a766c1
Robert Lange <robert.lange.81@gmail.com>
2024-04-28 17:44:39 +0200

    simulate session locking (load home page and then categories)

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -45,6 +45,7 @@ class PagesController extends AppController
      */
     public function display(string ...$path): ?Response
     {
+        // sleep(10);
         $config = Configure::read("test.key");
         pr($config);
         if (!$path) {
